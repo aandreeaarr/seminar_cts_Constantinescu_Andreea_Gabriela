79035891ed320a45b2b4038f6708bacdc30a9246
aandreeaarr <constantinescu.andreea06@gmail.com>
2023-04-28 00:44:48 +0300

    SEMINAR

--- seminar_6/src/builder/main/Main.java
+++ seminar_6/src/builder/main/Main.java
@@ -1,7 +1,24 @@
 package builder.main;
 
+import builder.clase.ContBancar;
+import builder.clase.ContBancarBuilder;
+import builder.clase.ContBuilder;
+import builder.clase.IBuilder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        IBuilder contBuilder = new ContBuilder();
+        ContBancar contBancar = contBuilder.setNume("Andreea").setCardAtasat(true).build();
+        System.out.println(contBancar);
+
+        ContBancar contBancar2 = contBuilder.setNume("Maria").setCardAtasat(true).build();
+        System.out.println(contBancar);
+        System.out.println(contBancar2);
+
+        IBuilder contBancarBuilder = new ContBancarBuilder();
+        ContBancar contBancar3 = contBancarBuilder.setInternetBanking(true).setNume("Carmen").build();
+        ContBancar contBancar4 = contBancarBuilder.setCardAtasat(true).setNume("Mircea").build();
+        System.out.println(contBancar3);
+        System.out.println(contBancar4);
     }
 }
